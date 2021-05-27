d8b204259356ad4912403c81f8aa78bec248bef6
Daryl <tsdaryl@gmail.com>
2021-05-27 10:53:59 -0700

    project setup

---
+++ static/js/mot.js
@@ -0,0 +1,5 @@
+const c1 = document.getElementById("object-layer").getContext("2d");
+const c2 = document.getElementById("overlay-layer").getContext("2d");
+
+c1.fillStyle = "white";
+c1.fillRect(50, 50, 100, 100);
