c24aa055688ede72467bcadbc9af5cf6db7396e4
Daryl <tsdaryl@gmail.com>
2021-05-27 18:04:30 -0700

    add OnscreenObj super object, static clearAll, checkCollision

--- static/js/mot.js
+++ static/js/mot.js
@@ -1,5 +1,23 @@
 const c1 = document.getElementById("object-layer").getContext("2d");
 const c2 = document.getElementById("overlay-layer").getContext("2d");
 
-const obj1 = new RectBlock(100, 100, "#fff", 200, 200);
-obj1.draw(c1);
+const obj1 = new RectBlock(100, 100, "#fff", 200, 200, { x: 1, y: 1 });
+const obj2 = new RectBlock(100, 100, "#ff3939", 500, 500, { x: 2, y: 4 });
+
+const onscreenObjects = [obj2, obj1];
+
+const animate = () => {
+  OnscreenObj.clearAll(c1);
+  c1.save();
+  for (const obj of onscreenObjects) {
+    if (obj.checkCollision(c1)) {
+      obj.resetPosition();
+    }
+    obj.translate(obj.velocity.x, obj.velocity.y);
+    obj.draw(c1);
+  }
+  window.requestAnimationFrame(animate);
+  c1.restore();
+};
+
+animate();
--- static/js/objectTypes.js
+++ static/js/objectTypes.js
@@ -1,12 +1,36 @@
-class RectBlock {
-  constructor(width, height, color, xPos, yPos) {
+class OnscreenObj {
+  constructor(width, height, color, xPos, yPos, velocity) {
     this.width = width;
     this.height = height;
     this.color = color;
     this.x = xPos;
     this.y = yPos;
+    this.originalCoords = { x: xPos, y: yPos };
+    this.velocity = velocity || { x: 0, y: 0 };
   }
 
+  translate(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  reposition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  resetPosition() {
+    this.x = this.originalCoords.x;
+    this.y = this.originalCoords.y;
+  }
+
+  static clearAll(ctx) {
+    const { width, height } = ctx.canvas;
+    ctx.clearRect(0, 0, width, height);
+  }
+}
+
+class RectBlock extends OnscreenObj {
   draw(ctx) {
     ctx.save();
     ctx.fillStyle = this.color;
@@ -14,8 +38,15 @@ class RectBlock {
     ctx.restore();
   }
 
-  translate(x, y) {
-    this.x += x;
-    this.y += y;
+  checkCollision(ctx, additionalObjs) {
+    if (
+      this.x > ctx.canvas.width ||
+      this.x + this.width > ctx.canvas.width ||
+      this.y > ctx.canvas.height ||
+      this.y + this.height > ctx.canvas.height
+    ) {
+      return true;
+    }
+    return false;
   }
 }
